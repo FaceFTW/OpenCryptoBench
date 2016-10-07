84c31cb754d5ad944cc4bfd43bf7fa4ff86cf05d
311286 <311286@427-D213-10-211.mcpsmd.org>
2016-10-07 09:47:36 -0400

    Switching Logger to java.util.logging functons
    *Removing package org.FaceStudios.OpenCryptoBench.Logger
    *Removed class org.FaceStudios.OpenCryptoBench.Logger.Logger

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Crypto/CryptoOps.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Crypto/CryptoOps.java
@@ -1,8 +1,18 @@
 package org.FaceStudios.OpenCryptoBench.Crypto;
 
+import java.io.PrintStream;
+import java.util.logging.Logger;
+
 public class CryptoOps {
 	//This class is mainly intended for a invoker method
 	//This will be used to attach a logger to a encryption invoked by the program
+	private static final Logger LOGGER = Logger.getLogger(CryptoOps.class.getName());
 	
+	public static void invokeCrypto(int method, int bitlen){
+		switch(method){
+		//This will invoke different methods depending on the parameters given.
+		
+		}
+	}
 	
 }

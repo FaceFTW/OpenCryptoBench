7142763f81bb526b6f2db75a9cb926d5df021559
FaceFTW <mcsack12@live.com>
2016-10-09 10:02:48 -0400

    Implementing AES Benchmark -Added Libraries Guava and Apache Commons Codec

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Crypto/Algorithms/AESCryptoOps.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Crypto/Algorithms/AESCryptoOps.java
@@ -1,9 +1,16 @@
 package org.FaceStudios.OpenCryptoBench.Crypto.Algorithms;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
-
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import org.FaceStudios.OpenCryptoBench.Crypto.CryptoObject;
-import org.FaceStudios.OpenCryptoBench.Crypto.CryptoOps;
+import org.apache.commons.codec.binary.Hex;
+import com.google.common.base.Stopwatch;
 
 public class AESCryptoOps {
 	//This is the redirect implementation for AES
@@ -11,20 +18,44 @@ public class AESCryptoOps {
 	//All methods should be static
 	
 	//Logger Implementaion
-	private static final Logger LOGGER = Logger.getLogger(DESCryptoOps.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(AESCryptoOps.class.getName());
+	//Stopwatch Implementation
+	private static Stopwatch stopwatch;
+	private static Cipher c;
 	
-	//Mode Constans enum
-	
-
-	
-	public static void performAES(CryptoOps.Mode mode, int bitlen, CryptoObject thing){
-		switch(mode){
-		case ENCRYPTION:          //Encryption
-			//Enter AES Code Here
-			break;
-		case DECRYPTION:
-			//Decryption Code Here
+	public static void performAES(int bitlen, CryptoObject thing){
+		LOGGER.info("Starting Stopwatch");
+		stopwatch = Stopwatch.createStarted();
+		LOGGER.info("Starting Encryption procedures for AES");
+		LOGGER.config("CryptoObject's input string is "+thing.getInput());
+		LOGGER.config("CryptoObject's SecretKey Object is "+Hex.encodeHexString(thing.getKey().getEncoded()));
+		LOGGER.config("Bit-size of key for encryption is "+bitlen+" bits");
+		LOGGER.info("Initializing Cipher as AES, "+bitlen+" bit key");
+		try {
+			c = Cipher.getInstance("AES/PKCS5 Padding");
+			c.init(Cipher.ENCRYPT_MODE, thing.getKey());
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+			LOGGER.severe("ERROR: Cipher object could not initialize with given algorithm and parameter");
+			e.printStackTrace();
 		}
-	}
-	
+		LOGGER.config("Success in initializing Cipher with given params");
+		LOGGER.info("Creating an output String");
+		String out = "";
+		LOGGER.config("Starting Encryption");
+		try {
+			out = Hex.encodeHexString(c.doFinal());
+		} catch (IllegalBlockSizeException | BadPaddingException e) {
+			LOGGER.severe("ERROR: Cipher could not execute encryption");
+			e.printStackTrace();
+		}
+		LOGGER.info("Success");
+		LOGGER.info("Output string is " +out);
+		LOGGER.info("Stopping Stopwatch for Encryption");
+		stopwatch.stop();
+		LOGGER.info("Time Elapsed for Encryption is "+stopwatch.elapsed(TimeUnit.MILLISECONDS) );
+		LOGGER.info("Resetting Stopwatch");
+		stopwatch.reset();
+		LOGGER.config("Success in resetting stopwatch");
+		}
 }
+	

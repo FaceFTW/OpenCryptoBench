798fcf8beac93953350354cd61f0b19e61cc7e29
311286 <311286@427-A307-10-177.mcpsmd.org>
2016-10-10 13:46:43 -0400

    Small Edits

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/MainGUI.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/MainGUI.java
@@ -4,15 +4,19 @@ import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JTextArea;
 
+@SuppressWarnings("unused")
+
 public class MainGUI {
 	//This class will serve as a mainstream GUI for the program
 	//All the swing components...
 	private JPanel mainFrame;
+	
 	private JScrollBar outPanel;
 	private JTextArea termOut;
 	
 	public MainGUI(){
-		//Essentially construct the window here
-		//Add Parameters if necessary
+		mainFrame = new JPanel();
+		outPanel = new JScrollBar();
+		termOut = new JTextArea();
 	}
 }

de028e20c8590fa6d58a7fc765352f0719c866d5
FaceFTW <FaceFTW@outlook.com>
2016-10-19 16:54:49 -0400

    Removed Unused import in MainFrame

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Executable/MainFrame.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Executable/MainFrame.java
@@ -1,6 +1,5 @@
 package org.FaceStudios.OpenCryptoBench.Executable;
 
-import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;

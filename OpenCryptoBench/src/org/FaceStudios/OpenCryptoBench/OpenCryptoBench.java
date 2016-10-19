197a4f448abb4f2100cc88e8675cf3b8514ca5ad
FaceFTW <FaceFTW@outlook.com>
2016-10-19 18:02:36 -0400

    I think its working...

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/OpenCryptoBench.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/OpenCryptoBench.java
@@ -1,21 +1,12 @@
 package org.FaceStudios.OpenCryptoBench;
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-
 import org.FaceStudios.OpenCryptoBench.Executable.NOGUI;
 
 public class OpenCryptoBench {
-	private static final Logger LOGGER = Logger.getLogger(OpenCryptoBench.class.getName());
+	//private static final Logger LOGGER = Logger.getLogger(OpenCryptoBench.class.getName());
 
 	public static void main(String[] args) {
-		try {
-			LOGGER.addHandler(new FileHandler("OpenCryptoBench.log"));
-		} catch (SecurityException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
+
 		/*if(args.length<0){
 			if(args[0].equals("nogui")){
 				if(args[1].equals("")){

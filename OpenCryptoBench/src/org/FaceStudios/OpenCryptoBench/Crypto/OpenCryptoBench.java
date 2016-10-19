2004a75725bf7511eb7f90918b2924c14f57f2f7
FaceFTW <FaceFTW@outlook.com>
2016-10-19 16:48:11 -0400

    Created Initializer Class
    -Created class OpenCryptoBench

---
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Crypto/OpenCryptoBench.java
@@ -0,0 +1,25 @@
+package org.FaceStudios.OpenCryptoBench.Crypto;
+
+import org.FaceStudios.OpenCryptoBench.Executable.NOGUI;
+
+public class OpenCryptoBench {
+
+	public static void main(String[] args) {
+		if(args.length<0){
+			if(args[0].equals("nogui")){
+				if(args[1].equals("")){
+					NOGUI.doNOGUI("default");
+				}
+				else{
+					NOGUI.doNOGUI(args[1]);
+				}
+			}
+			else{
+				System.out.println("Unfortunately, the GUI is still under developement.");
+				System.out.println("Please run this program with the \"nogui\" parameter");
+			}
+		}
+
+	}
+
+}

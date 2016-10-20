985f2b0322c06ef44ede7321e56d9931a2c15c6d
FaceFTW <FaceFTW@outlook.com>
2016-10-20 19:47:10 -0400

    Atttempt to implement cmd params

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/OpenCryptoBench.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/OpenCryptoBench.java
@@ -9,29 +9,30 @@ import org.FaceStudios.OpenCryptoBench.Executable.NOGUI;
 public class OpenCryptoBench {
 	public static final Logger GLOBALLOG = Logger.getLogger("org.FaceStudios.OpenCryptoBench");
 	public static void main(String[] args) {
-
-		/*if(args.length<0){
+		int x = 10;
+		if(args.length<0){
 			if(args[0].equals("nogui")){
-				if(args[1].equals("")){
-			*/
+				try{
+					
+					x = Integer.parseInt(args[1]);
+				}
+				catch(NumberFormatException ex){
+					ex.printStackTrace();
+				}
 				try {
 					GLOBALLOG.addHandler(new FileHandler("OpenCryptoBench.log"));
 				} catch (SecurityException | IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				NOGUI.doNOGUI("default",10);
+				NOGUI.doNOGUI("default",x);
 				
-				/*}
-				else{
-					NOGUI.doNOGUI(args[1]);
 				}
 			}
 			else{
 				System.out.println("Unfortunately, the GUI is still under developement.");
 				System.out.println("Please run this program with the \"nogui\" parameter");
 			}
-		}*/
 
 	}
 

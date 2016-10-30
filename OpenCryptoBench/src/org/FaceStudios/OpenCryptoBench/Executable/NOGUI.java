6aa2b94df6f7f14e0e9e1ad64b89196cb980eb4e
FaceFTW <FaceFTW@outlook.com>
2016-10-30 08:05:54 -0400

    Added system information to the filtered output

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Executable/NOGUI.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Executable/NOGUI.java
@@ -1,5 +1,8 @@
 package org.FaceStudios.OpenCryptoBench.Executable;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.logging.Logger;
 
 import org.FaceStudios.OpenCryptoBench.OpenCryptoBench;
@@ -46,8 +49,8 @@ public class NOGUI {
 		//Execute all different benchmarks
 		//AES
 		CryptoOps.invokeCrypto(Algorithm.AES, 128, thing);
-		//CryptoOps.invokeCrypto(Algorithm.AES, 192, thing);
-		//CryptoOps.invokeCrypto(Algorithm.AES, 256, thing);
+		CryptoOps.invokeCrypto(Algorithm.AES, 192, thing);
+		CryptoOps.invokeCrypto(Algorithm.AES, 256, thing);
 		//DES
 		CryptoOps.invokeCrypto(Algorithm.DES, 56, thing);
 		//3DES
@@ -57,67 +60,87 @@ public class NOGUI {
 		CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 32, thing);
 		CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 64, thing);
 		CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 128, thing);
-		//CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 256, thing);
-		//CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 384, thing);
-		//CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 448, thing);
+		CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 256, thing);
+		CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 384, thing);
+		CryptoOps.invokeCrypto(Algorithm.BLOWFISH, 448, thing);
 		//RC2
 		CryptoOps.invokeCrypto(Algorithm.RC2, 40, thing);
 		CryptoOps.invokeCrypto(Algorithm.RC2, 64, thing);
 		CryptoOps.invokeCrypto(Algorithm.RC2, 128, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC2, 256, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC2, 384, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC2, 448, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC2, 512, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC2, 640, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC2, 768, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC2, 896, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC2, 1024, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC2, 256, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC2, 384, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC2, 448, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC2, 512, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC2, 640, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC2, 768, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC2, 896, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC2, 1024, thing);
 		//RC4
 		CryptoOps.invokeCrypto(Algorithm.RC4, 40, thing);
 		CryptoOps.invokeCrypto(Algorithm.RC4, 64, thing);
 		CryptoOps.invokeCrypto(Algorithm.RC4, 128, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 256, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 384, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 448, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 512, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 640, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 768, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 896, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 1024, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 1156, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 1284, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 1412, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 1540, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 1668, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 1796, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 1924, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC4, 2048, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 256, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 384, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 448, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 512, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 640, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 768, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 896, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 1024, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 1156, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 1284, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 1412, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 1540, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 1668, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 1796, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 1924, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC4, 2048, thing);
 		//RC5
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 8, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 16, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 32, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 64, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 128, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 256, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 384, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 448, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 512, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 640, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 768, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 896, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 1024, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 1156, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 1284, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 1412, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 1540, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 1668, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 1796, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 1924, thing);
-		//CryptoOps.invokeCrypto(Algorithm.RC5, 2040, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 8, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 16, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 32, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 64, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 128, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 256, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 384, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 448, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 512, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 640, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 768, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 896, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 1024, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 1156, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 1284, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 1412, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 1540, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 1668, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 1796, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 1924, thing);
+		CryptoOps.invokeCrypto(Algorithm.RC5, 2040, thing);
 		LOGGER.info("####################################################################");
 		LOGGER.info("TEST ITERATION "+(x+1)+" COMPLETE");
 		LOGGER.info("####################################################################");
 		}
+		
+		PrintStream print = null;
+		try {
+			print = new PrintStream(new File("OpenCryptoBench.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		print.println("####################################################################");
+		print.println("SYSTEM INFORMATION");
+		print.println("####################################################################");
+		print.println("OS Name: "+System.getProperty("os.name"));
+		print.println("OS Architecture: "+ System.getProperty("os.arch"));
+		print.println("OS Version: "+System.getProperty("os.version"));
+		print.println("Java Version: "+System.getProperty("java.version"));
+		print.println("Java Vendor: "+System.getProperty("java.vendor"));
+		print.println("Username: "+System.getProperty("user.name"));
+		print.println("Working Directory: "+System.getProperty("user.dir"));
+		print.println("\n");
+		
+		
 		LOGGER.info("####################################################################");
 		LOGGER.info("FINISHED NOGUI OPERATION");
 		LOGGER.info("####################################################################");

5b8908d649374f2ca75a151d3779f58cad9fd0ae
FaceFTW <FaceFTW@outlook.com>
2016-12-20 10:17:28 -0500

    Started Rewrite of BlockCipherDataSet

---
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/BlockCipherDataSet.java
@@ -0,0 +1,17 @@
+package org.FaceStudios.OpenCryptoBench.Data;
+
+public class BlockCipherDataSet extends DataSet {
+	private String run;
+	private String algorithm;
+	private long keyGenTime;
+	private long encryptTime;
+	private long decryptTime;
+	
+	
+	@Override
+	public String toString() {
+		
+		return null;
+	}
+
+}

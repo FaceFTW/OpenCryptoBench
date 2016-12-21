23ed709fd3e0437cc61775e5ea9a1e655648c64d
FaceFTW <FaceFTW@outlook.com>
2016-12-21 18:36:01 -0500

    Rewriting all of the StreamCipherData classes
    -Finished Writing StreamCipherDataSet
    -Working on StreamCipherDataGroup doBenchmark() method

---
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/StreamCipherDataGroup.java
@@ -0,0 +1,124 @@
+package org.FaceStudios.OpenCryptoBench.Data;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import com.google.common.base.Stopwatch;
+
+public class StreamCipherDataGroup extends DataGroup<StreamCipherDataSet> {
+	private volatile ArrayList<StreamCipherDataSet> data;
+	private int runs;
+	private String algorithm;
+	private int bitlen;
+	private int ivlen;
+	
+	public enum StreamCipher{RC4, SALSA20, ISAAC, HC256, GRAINV1, GRAIN128}
+	private final BouncyCastleProvider PROVIDER = new BouncyCastleProvider();
+	
+	public StreamCipherDataGroup(int r, StreamCipher cipher){
+		data = new ArrayList<>();
+		runs = r;
+		
+		switch(cipher){
+		case GRAIN128:
+			algorithm = "Grain128";
+			bitlen = 0;
+			ivlen = 0;
+			break;
+		case GRAINV1:
+			algorithm = "Grainv1";
+			bitlen = 0;
+			ivlen = 0;
+			break;
+		case HC256:
+			algorithm = "HC256";
+			bitlen = 256;
+			ivlen = 0;
+			break;
+		case ISAAC:
+			algorithm = "ISAAC";
+			bitlen = 0;
+			ivlen = 0;
+			break;
+		case RC4:
+			algorithm = "RC4";
+			bitlen = 0;
+			ivlen = 0;
+			break;
+		case SALSA20:
+			algorithm = "Salsa20";
+			bitlen = 0;
+			ivlen = 0;
+			break;
+		default:
+			break;
+		
+		}
+	}
+	
+	@Override
+	public StreamCipherDataSet get(int x) {
+		return data.get(x);
+	}
+
+	@Override
+	public int getRuns() {
+		return runs;
+	}
+
+	@Override
+	public void add(StreamCipherDataSet t) {
+		data.add(t);
+	}
+
+	@Override
+	public void calcAggregate() {
+		long keygentime = 0, ivgentime = 0, encrypttime = 0, decrypttime = 0, totaltime = 0;
+		
+		for(int x = 0; x < data.size(); x++){
+			keygentime = keygentime + data.get(x).getKeyGenTime();
+			ivgentime = ivgentime + data.get(x).getIVGenTime();
+			encrypttime = encrypttime + data.get(x).getEncryptTime();
+			decrypttime = decrypttime + data.get(x).getDecryptTime();
+			totaltime = totaltime + data.get(x).getTotalTime();
+		}
+		
+		keygentime = keygentime / data.size();
+		ivgentime = ivgentime / data.size();
+		encrypttime = encrypttime / data.size();
+		decrypttime = decrypttime / data.size();
+		totaltime = totaltime / data.size();
+		
+		data.add(new StreamCipherDataSet("Aggregate", algorithm, bitlen, ivlen, keygentime, ivgentime, encrypttime, decrypttime, totaltime));
+	}
+
+	@Override
+	public void doBenchmark(String param) {
+		Stopwatch total = Stopwatch.createStarted();
+		
+		
+		//KEY GENERATION
+		Stopwatch keyGenTimer = Stopwatch.createStarted();
+		SecretKey secret;
+		KeyGenerator gen;
+		try {
+			gen = KeyGenerator.getInstance(algorithm, PROVIDER);
+			gen.init(bitlen);
+			secret = gen.generateKey();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		keyGenTimer.stop();
+		long keyGenTime = keyGenTimer.elapsed(TimeUnit.NANOSECONDS);
+		
+		
+		//IV GENERATION		
+	}
+
+}
---
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/StreamCipherDataSet.java
@@ -0,0 +1,66 @@
+package org.FaceStudios.OpenCryptoBench.Data;
+
+public class StreamCipherDataSet extends DataSet {
+	private String run;
+	private String algorithm;
+	private int bitlen;
+	private int ivlen;
+	private long keyGenTime;
+	private long ivGenTime;
+	private long encryptTime;
+	private long decryptTime;
+	private long totalTime;
+	
+	public StreamCipherDataSet(String r, String a, int b, int i, long k, long i2, long e, long d, long t){
+		run = r;
+		algorithm = a;
+		bitlen = b;
+		keyGenTime = k;
+		ivGenTime = i2;
+		encryptTime = e;
+		decryptTime = d;
+		totalTime = t;
+	}
+	
+	public String getRun(){
+		return run;
+	}
+	
+	public String getAlgorithm(){
+		return algorithm;
+	}
+	
+	public int getBitLength(){
+		return bitlen;
+	}
+	
+	public int getIVLength(){
+		return ivlen;
+	}
+	
+	public long getKeyGenTime(){
+		return keyGenTime;
+	}
+	
+	public long getIVGenTime(){
+		return ivGenTime;
+	}
+	
+	public long getEncryptTime(){
+		return encryptTime;
+	}
+	
+	public long getDecryptTime(){
+		return decryptTime;
+	}
+	
+	public long getTotalTime(){
+		return totalTime;
+	}
+	
+	@Override
+	public String toString() {
+		return run+","+algorithm+","+bitlen+","+keyGenTime+","+ivGenTime+","+encryptTime+","+decryptTime+","+totalTime;
+	}
+
+}

072dc2dd698899e2db24db5539795e88335c8267
FaceFTW <FaceFTW@outlook.com>
2016-12-21 10:30:39 -0500

    Almost finished with BlockCipherDataSet Benchmark

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/BlockCipherDataGroup.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/BlockCipherDataGroup.java
@@ -1,11 +1,18 @@
 package org.FaceStudios.OpenCryptoBench.Data;
 
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
-import org.bouncycastle.jcajce.provider.symmetric.AES;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import com.google.common.base.Stopwatch;
@@ -13,6 +20,7 @@ import com.google.common.base.Stopwatch;
 public class BlockCipherDataGroup extends DataGroup<BlockCipherDataSet>{
 	private volatile ArrayList<BlockCipherDataSet> data;
 	private int runs;
+	private int bitlen;
 	private String algorithm;
 	
 	public enum BlockCipher{AES, DES, DESEDE, BLOWFISH, TWOFISH, THREEFISH, RC2, RC5, SERPENT}
@@ -26,30 +34,39 @@ public class BlockCipherDataGroup extends DataGroup<BlockCipherDataSet>{
 		switch(cipher){
 		case AES:
 			algorithm = "AES";
+			bitlen = 256;
 			break;
 		case BLOWFISH:
 			algorithm = "Blowfish";
+			bitlen = 448;
 			break;
 		case DES:
 			algorithm = "DES";
+			bitlen = 56;
 			break;
 		case DESEDE:
 			algorithm = "DESede";
+			bitlen = 168;
 			break;
 		case RC2:
 			algorithm = "RC2";
+			bitlen = 256;
 			break;
 		case RC5:
 			algorithm = "RC5";
+			bitlen = 256;
 			break;
 		case SERPENT:
 			algorithm = "Serpent";
+			bitlen = 128;
 			break;
 		case THREEFISH:
 			algorithm = "Threefish";
+			bitlen = 256;
 			break;
 		case TWOFISH:
 			algorithm = "Twofish";
+			bitlen = 128;
 			break;
 		default:
 			break;
@@ -82,15 +99,57 @@ public class BlockCipherDataGroup extends DataGroup<BlockCipherDataSet>{
 
 	
 	public synchronized void doBenchmark(String param){
+		for(int x = 0 ; )
 		Stopwatch total = Stopwatch.createStarted();
 		
+		//KEY GENERATION BENCHMARK
 		Stopwatch keyGenTimer = Stopwatch.createStarted();
+		SecretKey key = null;
 		try {
 			KeyGenerator keyGen = KeyGenerator.getInstance(algorithm, PROVIDER);
-			keyGen.init();		
+			keyGen.init(bitlen);
+			key = keyGen.generateKey();
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
+		keyGenTimer.stop();
+		long keyGenTimeResult = keyGenTimer.elapsed(TimeUnit.NANOSECONDS);
+		
+		
+		//ENCRYPTION BENCHMARK
+		byte[] cipherText = null;
+		Cipher encrypt;
+		Stopwatch encryptTimer = Stopwatch.createStarted();
+		try {
+			encrypt = Cipher.getInstance(algorithm, PROVIDER);
+			encrypt.init(Cipher.ENCRYPT_MODE, key);
+			cipherText = encrypt.doFinal(param.getBytes(Charset.forName("US-ASCII")));
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+			e.printStackTrace();
+		}
+		encryptTimer.stop();
+		long encryptTimeResult = encryptTimer.elapsed(TimeUnit.NANOSECONDS);
+		
+		
+		//DECRYPTION BENCHMARK
+		Cipher decrypt;
+		Stopwatch decryptTimer = Stopwatch.createStarted();
+		try {
+			decrypt = Cipher.getInstance(algorithm, PROVIDER);
+			decrypt.init(Cipher.DECRYPT_MODE, key);
+			decrypt.doFinal(cipherText);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+			e.printStackTrace();
+		}
+		decryptTimer.stop();
+		long decryptTimeResult = decryptTimer.elapsed(TimeUnit.NANOSECONDS);
+		
+		
+		//Total Time Result
+		total.stop();
+		long totalTimeResult = total.elapsed(TimeUnit.NANOSECONDS);
+		
+		data.add(new BlockCipherDataSet());
 		
 	}
 	

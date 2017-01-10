732bb5c023de9b1a7a10d54f6c71802b7e1090ad
FaceFTW <FaceFTW@outlook.com>
2017-01-10 09:35:23 -0500

    Finished Implementation of the PublicKeyDataGroup classes

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/PublicKeyDataGroup.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/PublicKeyDataGroup.java
@@ -1,9 +1,20 @@
 package org.FaceStudios.OpenCryptoBench.Data;
 
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
-import javax.crypto.KeyGenerator;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 import org.FaceStudios.OpenCryptoBench.Data.PublicKeyDataSet.PublicKeyCipher;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -15,12 +26,14 @@ public class PublicKeyDataGroup extends DataGroup<PublicKeyDataSet> {
 	private int runs;
 	private int pubKeyBitLen;
 	private String algorithm;
+	private PublicKeyCipher cipher;
 	
 	private static final BouncyCastleProvider PROVIDER = new BouncyCastleProvider();
 	
-	PublicKeyDataGroup(int x, PublicKeyCipher cipher){
+	PublicKeyDataGroup(int x, PublicKeyCipher c){
 		data = new ArrayList<>();
 		runs = x;
+		cipher = c;
 		switch(cipher){
 		case ELGAMAL:
 			algorithm = "ElGamal";
@@ -71,23 +84,63 @@ public class PublicKeyDataGroup extends DataGroup<PublicKeyDataSet> {
 
 	@Override
 	public synchronized void doBenchmark(String param) {
-		Stopwatch total = Stopwatch.createStarted();
+		
+		for(int x = 0; x< runs; x++){
+			Stopwatch total = Stopwatch.createStarted();
+		
 		
 		//KEY GENERATION
 		Stopwatch pubKeyGenTimer = Stopwatch.createStarted();
-		KeyGenerator gen;
-		RSAPublicKey key;
+		SecureRandom rand;
+		KeyPairGenerator gen;
+		KeyPair pair;
+		PublicKey key = null;
 		try {
-			gen = KeyGenerator.getInstance(algorithm, PROVIDER);
-			gen.init(pubKeyBitLen);
-			key = gen.generateKey();		
-		} catch (NoSuchAlgorithmException e) {
+			gen = KeyPairGenerator.getInstance(algorithm, PROVIDER);
+			rand = SecureRandom.getInstance("SHA1PRNG", "SUN");
+			gen.initialize(pubKeyBitLen, rand);
+			pair = gen.generateKeyPair();
+			key = pair.getPublic();
+		} catch (NoSuchAlgorithmException | NoSuchProviderException e) {
 			e.printStackTrace();
 		}
+		pubKeyGenTimer.stop();
+		long pubKeyGenTime = pubKeyGenTimer.elapsed(TimeUnit.NANOSECONDS);
 		
 		
+		//ENCRYPTION
+		Stopwatch encryptTimer = Stopwatch.createStarted();
+		Cipher c;
+		byte[] ciphertext = null;
+		try {
+			c = Cipher.getInstance(algorithm, PROVIDER);
+			c.init(Cipher.ENCRYPT_MODE, key);
+			ciphertext = c.doFinal(param.getBytes(Charset.forName("US-ASCII")));
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
+			e.printStackTrace();
+		}
+		encryptTimer.stop();
+		long encryptTime = encryptTimer.elapsed(TimeUnit.NANOSECONDS);
 		
 		
+		//DECRYPTION
+		Stopwatch decryptTimer = Stopwatch.createStarted();
+		Cipher d;
+		try {
+			d = Cipher.getInstance(algorithm, PROVIDER);
+			d.init(Cipher.DECRYPT_MODE, key);
+			d.doFinal(ciphertext);
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException e) {
+			e.printStackTrace();
+		}
+		decryptTimer.stop();
+		long decryptTime = decryptTimer.elapsed(TimeUnit.NANOSECONDS);
+		
+		total.stop();
+		long totalTime = total.elapsed(TimeUnit.NANOSECONDS);
+		
+		data.add(new PublicKeyDataSet(Integer.toString(x+1), cipher, pubKeyGenTime, encryptTime, decryptTime, totalTime));
+		}
 	}
 
 	

a3bf90c6297c032899047b4817e33c1bce58be7f
FaceFTW <FaceFTW@outlook.com>
2017-08-20 09:05:09 -0400

    More Work on Message Digest Benchmarking
    - Added conditional in constructor of MessageDigestDataGroup to identify
    which method to test (NOT FINISHED)

--- OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/MessageDigest/MessageDigestDataGroup.java
+++ OpenCryptoBench/src/org/FaceStudios/OpenCryptoBench/Data/MessageDigest/MessageDigestDataGroup.java
@@ -1,22 +1,95 @@
 package org.FaceStudios.OpenCryptoBench.Data.MessageDigest;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 import org.FaceStudios.OpenCryptoBench.Data.DataGroup;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import com.google.common.base.Stopwatch;
+
 public class MessageDigestDataGroup extends DataGroup<MessageDigestDataSet>{
 	
 	private static final BouncyCastleProvider PROVIDER = new BouncyCastleProvider();
 	
+	public enum MessageDigestMethod {SHA1, SHA256, SHA3, MD2, MD4, MD5, KECCAK, BLAKE2B, GOST3411, RIPEMD128, RIPEMD256, SKEIN, SM3, TIGER, WHIRLPOOL};
+	
 	ArrayList<MessageDigestDataSet> data;
 	int runs;
 	String input;
+	String algorithm;
+	int bitsize;
 	
-	public MessageDigestDataGroup(int r, String i){
+	public MessageDigestDataGroup(int r, String i, MessageDigestMethod m, int b){
 		data = new ArrayList<>();
 		runs = r;
 		input = i;
+		switch (m){
+		case BLAKE2B:
+			algorithm = "Blake2b";
+			bitsize = 0;
+			break;
+		case GOST3411:
+			algorithm = "GOST3411";
+			bitsize = 0;
+			break;
+		case KECCAK:
+			algorithm = "Keccak";
+			bitsize = 0;
+			break;
+		case MD2:
+			algorithm = "MD2";
+			bitsize = 0;
+			break;
+		case MD4:
+			algorithm = "MD4";
+			bitsize = 0;
+			break;
+		case MD5:
+			algorithm = "MD5";
+			bitsize = 0;
+			break;
+		case RIPEMD128:
+			algorithm = "RIPEMD128";
+			bitsize = 128;
+			break;
+		case RIPEMD256:
+			algorithm = "RIPEMD256";
+			bitsize = 256;
+			break;
+		case SHA1:
+			algorithm = "SHA1";
+			bitsize = 128;				//TODO Please Check and ensure this is correct
+			break;
+		case SHA256:
+			algorithm = "SHA256";
+			bitsize = 256;
+			break;
+		case SHA3:
+			algorithm = "SHA3";
+			bitsize = 0;
+			break;
+		case SKEIN:
+			algorithm = "Skein";
+			bitsize = 0;
+			break;
+		case SM3:
+			algorithm = "SM3";
+			bitsize = 0;
+			break;
+		case TIGER:
+			algorithm = "Tiger";
+			bitsize = 0;
+			break;
+		case WHIRLPOOL:
+			algorithm = "Whrilpool";
+			bitsize = 0;
+			break;
+		default:
+			break;
+		
+		}
 	}
 	
 	@Override
@@ -58,7 +131,17 @@ public class MessageDigestDataGroup extends DataGroup<MessageDigestDataSet>{
 
 	@Override
 	public void doBenchmark(String param) {
-		
+		for(int x = 1; x <= runs; x++){
+			Stopwatch total = Stopwatch.createStarted();
+			
+			//HASH TIMER
+			try {
+				MessageDigest md = MessageDigest.getInstance(algorithm, PROVIDER);
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	
